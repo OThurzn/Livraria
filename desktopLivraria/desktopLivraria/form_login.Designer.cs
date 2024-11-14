0a11697ba93d64ed9893ecaa38b5be9d88abc7ed
2º ETIM - Desenvolvimento de Sistemas <ds-etim-2@servidor.acad>
2024-11-14 15:21:57 -0300

    vAlpha

---
+++ desktopLivraria/desktopLivraria/form_login.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace desktopLivraria
+{
+    partial class fomr_login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.directoryEntry1 = new System.DirectoryServices.DirectoryEntry();
+            this.txt_user = new System.Windows.Forms.TextBox();
+            this.txt_senha = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.25F);
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(836, 118);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 44);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Login";
+            // 
+            // txt_user
+            // 
+            this.txt_user.Location = new System.Drawing.Point(790, 222);
+            this.txt_user.Multiline = true;
+            this.txt_user.Name = "txt_user";
+            this.txt_user.Size = new System.Drawing.Size(221, 46);
+            this.txt_user.TabIndex = 1;
+            // 
+            // txt_senha
+            // 
+            this.txt_senha.Location = new System.Drawing.Point(790, 322);
+            this.txt_senha.Multiline = true;
+            this.txt_senha.Name = "txt_senha";
+            this.txt_senha.PasswordChar = '*';
+            this.txt_senha.Size = new System.Drawing.Size(221, 48);
+            this.txt_senha.TabIndex = 2;
+            // 
+            // fomr_login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::desktopLivraria.Properties.Resources.Sem_título1;
+            this.ClientSize = new System.Drawing.Size(1264, 793);
+            this.ControlBox = false;
+            this.Controls.Add(this.txt_senha);
+            this.Controls.Add(this.txt_user);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "fomr_login";
+            this.Text = "form_login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.DirectoryServices.DirectoryEntry directoryEntry1;
+        private System.Windows.Forms.TextBox txt_user;
+        private System.Windows.Forms.TextBox txt_senha;
+    }
+}
+
---
+++ desktopLivraria/desktopLivraria/form_login.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace desktopLivraria
+{
+    public partial class fomr_login : Form
+    {
+        public fomr_login()
+        {
+            InitializeComponent();
+        }
+    }
+}
